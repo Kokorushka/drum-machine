e97dfca10141c06ba0ea7cf329b776fb35348c7c
Alexandra <kokosha117@gmail.com>
2021-02-04 12:50:12 +0300

    simple drum machine created

---
+++ index.js
@@ -0,0 +1,20 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+};
+
+function removeTransition (e) {
+  console.log(e);
+  if (e.propertyName !== 'transform') {
+    return;
+  }
+  console.log(this);
+  this.classList.remove('playing');
+};
+window.addEventListener('keydown', playSound);
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
